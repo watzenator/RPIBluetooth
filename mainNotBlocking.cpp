a0b2cc526c739d5f6cef6500de18b4de76b3f39c
Suirevax <richard.janssen@student.hu.nl>
2019-03-28 14:15:56 +0100

    fixed a lot of stuff

--- mainNotBlocking.cpp
+++ mainNotBlocking.cpp
@@ -1,6 +1,9 @@
+#inlucde "BrickPi3.cpp"
 #include "BluetoothSocket.h"
-#include <unistd.h>
-using namespace std;
+#include <stdio.h>      // for printf
+#include <unistd.h>     // for usleep
+#include <signal.h>     // for catching exit signals
+#include <iostream>
 
 void forward(int motorspeed){
 	BP.set_motor_power(PORT_C, motorspeed);
@@ -22,9 +25,22 @@ void brake(){
 	BP.set_motor_power(PORT_B, 0);
 }
 
+// Signal handler that will be called when Ctrl+C is pressed to stop the program
+void exit_signal_handler(int signo){
+	if(signo == SIGINT){
+		BP.reset_all();    // Reset everything so there are no run-away motors
+		exit(-2);
+	}
+}
+
 
 
 int main() {
+	signal(SIGINT, exit_signal_handler); // register the exit function for Ctrl+C
+
+	int32_t EncoderC = BP.offset_motor_encoder(PORT_C, BP.get_motor_encoder(PORT_C));
+	int32_t EncoderB = BP.offset_motor_encoder(PORT_B, BP.get_motor_encoder(PORT_B));
+
 	BluetoothServerSocket serversock(2, 1);  //2 is het channel-number
 	cout << "listening" << endl;
 	while(true) {
